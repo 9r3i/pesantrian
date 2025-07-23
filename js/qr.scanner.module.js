82d825d452a6eee71c3e22e5e56c42fa2df1b725
9r3i <luthfie@y7mail.com>
2025-07-24 03:39:12 +0700

    add qr.scanner.module.js

---
+++ js/qr.scanner.module.js
@@ -0,0 +1,3 @@
+/*import QrScanner from "https://9r3i.web.id/files/text/javascript/qr-scanner.min.js";*/
+import QrScanner from "https://rawfo.vercel.app/9r3i/qrlogin.js/master/scanner/qr-scanner.min.js";
+window.QrScanner=QrScanner;
