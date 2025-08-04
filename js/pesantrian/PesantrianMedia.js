25489d5e405ef89aab50704bc8bb2cca52181686
9r3i <luthfie@y7mail.com>
2025-08-05 05:19:41 +0700

    add pesantrian.media.js

---
+++ js/pesantrian/PesantrianMedia.js
@@ -0,0 +1,216 @@
+
+/* PesantrianMedia --- removed */
+;function PesantrianMedia(app){
+this.app=app;
+this.aliasData={
+  name:'Nama Barang',
+  item_code:'Kode Barang',
+  quantity:'Quantity',
+  condition:'Kondisi',
+};
+this.table=_Pesantrian.table;
+this.row=_Pesantrian.row;
+this.rowHead=_Pesantrian.rowHead;
+
+this.init=async function(){
+  this.app.menuButton.remove();
+  
+  /* inside apps */
+  let apps=[],
+  appsx=[
+    {
+      name:'inventory',
+      title:'Inventory',
+      callback:function(e){
+        _PesantrianMedia.tableInventory();
+      }
+    }
+  ],
+  adminApps=_Pesantrian.buildApps(apps);
+  this.clearBody();
+  this.app.body.append(adminApps);
+  adminApps.show();
+  
+  /* goback button */
+  this.app.body.append(
+    _Pesantrian.goback(async ()=>{
+      await _Pesantrian.load('apps.html');
+    })
+  );
+  /* put the object to global scope */
+  window._PesantrianMedia=this;
+  /* next execution */
+  if(typeof this.app.next==='string'
+    &&typeof this[this.app.next]==='function'){
+    let args=Array.isArray(this.app.args)?this.app.args:[];
+    return this[this.app.next].apply(this,args);
+  }
+};
+
+this.addItem=function(){
+  this.clearBody();
+  let lines=this.templateItem(),
+  row=this.rowHead('INPUT BARANG',2),
+  table=this.table();
+  table.append(row);
+  row=this.row('');
+  for(let key in lines){
+    let value=lines[key],
+    val=document.createElement('input');
+    val.name=key;
+    val.type=key=='quantity'?'number':'text';
+    val.placeholder=this.alias(key)+'...';
+    val.classList.add('kitchen-find');
+    row=this.row(this.alias(key),val);
+    table.append(row);
+  }
+  
+  this.app.body.append(table);
+  /* goback button */
+  this.app.body.append(
+    _Pesantrian.goback(async ()=>{
+      await this.tableInventory();
+    })
+  );
+  
+  /* button */
+  let div=document.createElement('div'),
+  btn=document.createElement('input');
+  btn.type='submit';
+  btn.value='Simpan';
+  div.append(btn);
+  div.classList.add('grid');
+  div.classList.add('grid-max');
+  this.app.body.append(div);
+  btn.onclick=async ()=>{
+    let data=_Pesantrian.formSerialize();
+    delete data.data;
+    if(false){
+      alert(_Pesantrian.parser.likeJSON(data,3));
+      return;
+    }
+    let innerQuery=_Pesantrian.buildQuery(data),
+    query='insert into "media_inventory" '+innerQuery,
+    queries=[
+      query,
+    ].join(';'),
+    loader=_Pesantrian.loader(),
+    res=await _Pesantrian.request('queries',queries),
+    error=false;
+    loader.remove();
+    if(!res){
+      error='Kesalahan tidak diketahui.';
+    }else if(res.hasOwnProperty('error')){
+      error=res.error;
+    }
+    _Pesantrian.notif(
+      res&&!error?'Tersimpan':error,
+      res&&!error?'success':'error'
+    );
+    if(!error){
+      btn.disabled=true;
+      setTimeout(async ()=>{
+        await this.tableInventory();
+      },1600);
+    }
+  };
+};
+this.tableInventory=async function(){
+  this.clearBody();
+  /* goback button */
+  this.app.body.append(
+    _Pesantrian.goback(async ()=>{
+      await this.init();
+    })
+  );
+  let loader=_Pesantrian.loader(),
+  queries=[
+    'select * from media_inventory',
+  ].join(';'),
+  data=await _Pesantrian.request('queries',queries),
+  items=data[0],
+  add=document.createElement('input'),
+  row=this.rowHead('MEDIA INVENTORY',5),
+  table=this.table();
+  loader.remove();
+  table.append(row);
+  row=this.row('Kode','Nama Barang','Qty',
+    this.alias('condition'),add);
+  row.classList.add('tr-head');
+  table.append(row);
+  add.type='submit';
+  add.value='Tambah';
+  add.classList.add('button-add');
+  add.onclick=function(){
+    _PesantrianMedia.addItem();
+  };
+  let find=document.createElement('input');
+  find.classList.add('kitchen-find');
+  find.type='text';
+  find.placeholder='Cari Nama Barang...';
+  find.onkeyup=function(e){
+    let rg=new RegExp(this.value,'i'),
+    nm=document.querySelectorAll('tr[data-name]');
+    for(let i=0;i<nm.length;i++){
+      if(nm[i].dataset.name.match(rg)){
+        nm[i].style.removeProperty('display');
+      }else{
+        nm[i].style.display='none';
+      }
+    }
+  };
+  row=this.row('',find,'','','');
+  table.append(row);
+  
+  for(let item of items){
+    let del=document.createElement('input');
+    del.type='submit';
+    del.value='Hapus';
+    del.classList.add('button-delete');
+    del.classList.add('extra-high');
+    del.dataset.id=''+item.id;
+    del.dataset.name=''+item.name;
+    del.onclick=function(){
+      let el=document.getElementById('item-'+this.dataset.id);
+      if(!el){return;}
+      _Pesantrian.confirm('Hapus barang?',
+        this.dataset.name,async (yes)=>{
+        if(!yes){return;}
+        this.value='Menghapus...';
+        this.disabled=true;
+        let query='delete from media_inventory where id='
+          +this.dataset.id,
+        res=await _Pesantrian.request('query',query);
+        el.remove();
+      });
+    };
+    row=this.row(item.item_code,item.name,item.quantity,item.condition,del);
+    row.dataset.name=item.name;
+    row.id='item-'+item.id;
+    table.append(row);
+  }
+  
+  this.app.body.append(table);
+};
+
+this.templateItem=function(){
+  return {
+    name:'',
+    quantity:0,
+    item_code:'',
+    condition:'',
+  };
+};
+this.clearBody=function(){
+  this.app.body.innerHTML='';
+  this.app.body.scrollTo(0,0);
+  this.app.hideMenu();
+};
+this.alias=function(key){
+  return this.aliasData.hasOwnProperty(key)
+    ?this.aliasData[key]:key;
+};
+return this.init();
+};
+
+
